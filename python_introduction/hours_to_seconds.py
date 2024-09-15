3b4e5db4e5b07ba69c08753e317cda9b478979b6
EdwardMogoane <digits955@gmail.com>
2024-09-15 21:07:17 +0200

    fixed the seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -3,7 +3,7 @@ hours = 2 # number of hours
 seconds= 3600 # number of seconds in an hour
 
 # Calculating hours to seconds
-seconds = hours * seconds # multiplies the hours with seconds
+seconds = hours * 3600 # multiplies the hours with seconds
 
 # printing the value
 print(f"{hours} hours is equal to {seconds} seconds")
