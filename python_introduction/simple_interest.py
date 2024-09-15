615c9eff1c06e79406f5402ee5a9a6e0976c3b78
EdwardMogoane <digits955@gmail.com>
2024-09-15 20:29:59 +0200

    aded a interest file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Define variables
+principal = 1000 # principal amount $1000
+rate = 0.05 # 5% annual interest rate 
+time = 3 #3 yeara of investment
+
+# Calculating simple interest
+interest = principal * rate * time
+
+#Print calculated interest
+print("The simple interest is:", interest)
