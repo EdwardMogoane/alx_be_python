b884828b44c5b4e08efbd0140a044dabfb1f4696
EdwardMogoane <digits955@gmail.com>
2024-09-15 20:59:58 +0200

    aded a file that calculates hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+# Defining variables
+hours = 2 # number of hours
+seconds_in_hour= 3600 # number of seconds in an hour
+
+# Calculating hours to seconds
+total_seconds = hours * seconds_in_hour # multiplies the hours with seconds
+
+# printing the value
+print(f"{hours} hours is equal to {total_seconds} seconds")
