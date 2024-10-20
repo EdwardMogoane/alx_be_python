7fdb6a21101751364a6baed346e991cdc70a773a
EdwardMogoane <digits955@gmail.com>
2024-10-20 16:47:07 +0200

    fixed the issue

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -10,10 +10,15 @@ class Calculator:
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
 
-result_add = Calculator.add(5, 10)
-result_multiply = Calculator.multiply(5, 10)
+def main():
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
 
-print(f"The sum is: {result_add}")
-print(f"The product is: {result_multiply}")
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
+
 
 
